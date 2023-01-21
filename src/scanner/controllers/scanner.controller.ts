c54de2b2c9b3fbfacc56acb8fa84d108b4478a35
misha <tymish1@gmail.com>
2023-01-21 09:16:31 +0200

    added information by each scanner - methods and write README

--- src/scanner/controllers/scanner.controller.ts
+++ src/scanner/controllers/scanner.controller.ts
@@ -15,11 +15,18 @@ import { ScannerService } from '../services/scanner.service';
 export class ScannerController {
   constructor(private scannerService: ScannerService) {}
 
+  /**
+   * get /api/scanner - for show all scanner save by scanner collection
+   */
   @Get()
   async findAll(): Promise<Scanner[]> {
     return await this.scannerService.findAll();
   }
 
+  /**
+   * post /api/scanner - for create new document into scanner collection by body parameters
+   * @param body: Scanner
+   */
   @Post()
   async create(
     @Body(new ValidationPipe({ transform: true })) body: Scanner,
@@ -27,11 +34,20 @@ export class ScannerController {
     return await this.scannerService.create(body);
   }
 
+  /**
+   * get /api/scanner/:id - for show scanner document by id
+   * @param id: string
+   */
   @Get(':id')
   async findItem(@Param('id') id: string): Promise<Scanner> {
     return await this.scannerService.findOne(id);
   }
 
+  /**
+   * path /api/scanner/:id - for find scanner document by id and update field by body parameters
+   * @param id: string
+   * @param body: Scanner
+   */
   @Patch(':id')
   async updateItem(
     @Param('id') id: string,
@@ -40,6 +56,10 @@ export class ScannerController {
     return await this.scannerService.updateOne(id, body);
   }
 
+  /**
+   * delete /api/scanner/:id - for remove scanner document by id
+   * @param id: string
+   */
   @Delete(':id')
   async deleteItem(@Param('id') id: string): Promise<Scanner> {
     return await this.scannerService.deleteOne(id);
--- src/scanner/services/scanner.service.ts
+++ src/scanner/services/scanner.service.ts
@@ -9,22 +9,42 @@ export class ScannerService {
     @InjectModel(Scanner.name) private scannerModel: Model<ScannerDocument>,
   ) {}
 
+  /**
+   * Method create new scanner collection document and return this document
+   * @param scanner: Scanner
+   */
   async create(scanner: Scanner): Promise<Scanner> {
     return await this.scannerModel.create(scanner);
   }
 
+  /**
+   * Method return all documents with collection - scanner and sorted by price descending
+   */
   async findAll(): Promise<Scanner[]> {
     return await this.scannerModel.find({}).exec();
   }
 
+  /**
+   * Method return one scanner document by id
+   * @param id: string
+   */
   async findOne(id: string): Promise<Scanner> {
     return await this.scannerModel.findById(id).exec();
   }
 
+  /**
+   * Method remove one scanner document by id and return this document
+   * @param id: string
+   */
   async deleteOne(id: string): Promise<Scanner> {
     return await this.scannerModel.findByIdAndRemove(id).exec();
   }
 
+  /**
+   * Method find one scanner document by id and update fields by body parameters. After update method return updated document
+   * @param id: string
+   * @param body: Scanner
+   */
   async updateOne(id: string, body: Scanner): Promise<Scanner> {
     return await this.scannerModel
       .findOneAndUpdate({ _id: id }, { $set: body })
