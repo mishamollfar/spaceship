0865bdbe80c6a2bd741ac7d8e0a2931868849094
misha <tymish1@gmail.com>
2023-01-21 10:40:04 +0200

    added types, description and instruction to method getVesselConfigs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { QueryVesselConfigsDto } from './interfaces/query-vessel-configs.dto';
+import { VesselConfigs } from './interfaces/vessel-configs';
 
 @Controller()
 export class AppController {
@@ -10,8 +12,19 @@ export class AppController {
     return this.appService.getHello();
   }
 
+  /**
+   * get /api/getVesselConfigs - array of suitable configuration object
+   * @param query: QueryVesselConfigsDto - {
+   * max_price: number - Maximum budget for a spaceship, in M $;
+   * journey_distance: number - Minimum required Journey Distance, in Light years;
+   * weight?: number - optional; Maximum allowed Total Spaceship Weight
+   * }
+   * @return {VesselConfigs[]}
+   */
   @Get('getVesselConfigs')
-  getVesselConfigs() {
-    return this.appService.getVesselConfigs();
+  getVesselConfigs(
+    @Query() query: QueryVesselConfigsDto,
+  ): Promise<VesselConfigs[]> {
+    return this.appService.getVesselConfigs(query);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { QueryVesselConfigsDto } from './interfaces/query-vessel-configs.dto';
+import { VesselConfigs } from './interfaces/vessel-configs';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Api, that will serve calculated suitable spaceship configurations based on the various requirements.';
   }
 
-  getVesselConfigs() {
-    return [];
+  /**
+   * Method return array of suitable configuration objects, sorted by Price (DESC). Limit to 10 records
+   * @param query: QueryVesselConfigsDto - {max_price: number - Maximum budget for a spaceship, in M $; journey_distance: number - Minimum required Journey Distance, in Light years; weight?: number - Maximum allowed Total Spaceship Weight
+   * @return {VesselConfigs[]}
+   */
+  getVesselConfigs(query: QueryVesselConfigsDto): Promise<VesselConfigs[]> {
+    // select record with all collection
+    // gradation by type spaceship
+    // equal total parameters and create object to response
+    // select spaceship configuration to response with accept input parameters
+    // sorting response
+    // apply limit to return response
+    return undefined;
   }
 }
